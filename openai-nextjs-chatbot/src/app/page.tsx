601856143860cedcc6224cd80d75aa40c6877e17
Doug <wanderlustking11@gmail.com>
2025-04-19 18:21:30 -0400

    Create Route Handler; add textarea component

--- openai-nextjs-chatbot/src/app/page.tsx
+++ openai-nextjs-chatbot/src/app/page.tsx
@@ -1,4 +1,23 @@
+'use client'
+import { Textarea } from '@/components/ui/textarea';
+import { useChat } from '@ai-sdk/react';
 
 export default function Home() {
-  return <main>Hello World</main>;
+  const { messages, handleSubmit, input, handleInputChange } = useChat();
+
+  return (
+    <main className='fixed h-full w-full bg-muted'>
+      <div className="container h-full w-full flex flex-col py-8">
+        <div className='flex-1 overflow-y-auto'></div>
+        <form onSubmit={handleSubmit} className='mt-auto relative'>
+          <Textarea 
+            className='w-full text-lg'
+            placeholder='Say something'
+            value={input}
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+    </main>
+  )
 }
