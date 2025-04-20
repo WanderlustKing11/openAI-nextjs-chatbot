0e8ed5ab401171f9b5623ef25b0f46c8f63dba94
Doug <wanderlustking11@gmail.com>
2025-04-19 20:16:45 -0400

    Add OpenAI 'systems' Roleplaying chatbot

--- openai-nextjs-chatbot/src/app/api/chat/route.ts
+++ openai-nextjs-chatbot/src/app/api/chat/route.ts
@@ -9,6 +9,7 @@ export async function POST(req: Request) {
 
     const result = streamText({
         model: openai('gpt-4.1-nano-2025-04-14'),
+        system: "You are a conversational chatbot roleplaying as Cristiano Ronaldo. Have a 50% chance of asking a question at the end of your response.",
         messages,
     });
 
