b174e0fc20caaa986a6ec264e09d852bdddcb10e
Doug <wanderlustking11@gmail.com>
2025-04-27 21:42:21 -0400

    Add tailwind.config.js

---
+++ openai-nextjs-chatbot/tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            backgroundImage: {
+                'mobile-hero': "url('/public/ronaldo-mobile.jpg')",
+                'desktop-hero': "url('/public/ronaldo-desktop.jpg)",
+            }
+        }
+    },
+}
