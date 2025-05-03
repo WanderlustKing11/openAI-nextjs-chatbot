fcf1729f0a488661ace153ec9ba13a2f8f5dc691
Doug <wanderlustking11@gmail.com>
2025-05-02 20:50:18 -0400

    fix bug: add new heros

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,18 @@ const VALID_CODES = [
     process.env.CR7_CODE!,
     process.env.MESSI_CODE!,
     process.env.LBJ_CODE!,
+    process.env.TB_CODE!,
+    process.env.SHOHEI_CODE!,
+    process.env.JUDGE_CODE!,
+    process.env.MBAPPE_CODE!,
+    process.env.CURRY_CODE!,
+    process.env.GOKU_CODE!,
+    process.env.YODA_CODE!,
+    process.env.PICKLE_RICK_CODE!,
+    process.env.ALUCARD_CODE!,
+    process.env.BRUCE_LEE_CODE!,
+    process.env.MJ_CODE!,
+    process.env.KHALEESI_CODE!,
 ];
 
 export function middleware(req: NextRequest) {
